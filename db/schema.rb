741fa3d77325f90a3b52943c4801210030363a06
Julian Arnold <julian_arnold@hotmail.com>
2015-11-02 21:59:30 +0000

    added strong parameters to articles controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151003163644) do
+ActiveRecord::Schema.define(version: 20151014211206) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "widgets", force: :cascade do |t|
     t.string   "name"
