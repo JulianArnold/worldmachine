e3e4de52a5f6cc7dfbb242dcbe281b5fee9b7166
JulianArnold <julian_arnold@hotmail.com>
2017-01-26 22:17:35 +0000

    added receive method to contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,31 @@
 class ContactMailer < ApplicationMailer
   default from: 'julian.arnold123@gmail.com'
 
-  def welcome_email(contact)
+  def welcome_email(contact, email_body)
     @contact = contact
       email_with_name = %("#{@contact.name}" <#{@contact.email}>)
     @url = 'http://localhost:3000/contacts/new'
-    mail(to: email_with_name, subject: 'Welcome to World Machine Technology!')
+    mail(to: email_with_name,
+         body: email_body,
+         content_type: "text/html",
+         subject: 'Welcome to World Machine Technology!')
+  end
 
+
+  def receive(email)
+    page = Page.find_by(address: email.to.first)
+    page.emails.create(
+        subject: email.subject,
+        body: email.body
+    )
+
+    if email.has_attachments?
+        email.attachments.each do |attachment|
+        page.attachments.create({
+                                    file: attachment,
+                                    description: email.subject
+                                })
+      end
+    end
   end
 end
