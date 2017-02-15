12e2bb55d01aed4fa0930c8239337fda9a4fc109
JulianArnold <julian_arnold@hotmail.com>
2017-02-15 20:00:56 +0000

    port number changed for production deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'world-machine.herokuapp.com',
+      port:                 25,
+      domain:               'herokuapp.com',
       user_name:            ENV['GMAIL_USER_NAME'],
       password:             ENV['GMAIL_PASSWORD'],
       authentication:       'plain',
